dc44a12be8ba2c05baab8083edaf5432ae83ee99
Даня Ходосевич <114701605+SpaghettSauce@users.noreply.github.com>
2024-10-13 18:24:42 +0200

    fib matrix fuck

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -9,27 +9,28 @@ int main(){
 
 //Binet
     float b;
-    b = (pow((1+sqrt(5))/2,n)) - (pow((1-sqrt(5))/2,n));
-    cout<<"Через формулу Бине: "<<int(b/sqrt(5));
+    b = (pow(((1+sqrt(5))/2),n)) - (pow(((1-sqrt(5))/2),n));
+    cout<<"Через формулу Бине: "<<b/sqrt(5)<<endl;
 
 //Matrix 
- int arr[2][2] = {{0,1},{1,1}};
- int def[2][2];
- for (int q=0;q<n;q++){
-    for (int i=0; i<2; i++){
-        for (int j=0; j<2; j++){
-            def[i][j] = arr[i][j] * arr[j][i];
-             cout<<"\t"<<def[i][j];
+ int arr[2][2] = {0,1,1,1};
+ int def[2][2] = {0,0,0,1};
+ for (int q=0; q<n; q++){
+    for (int i=0; i<2; i++){           
+        for (int j=0; j<2; j++){                                         
+            def[i][j] += arr[i][j] + 1;
         }
     }
- }
+}
 
-/*for (int i=0;i<2;i++){
+for (int i=0;i<2;i++){
     for (int j=0; j<2; j++){
         cout<<"\t"<<def[i][j];
     }
     cout<<endl;
-}*/
+}
+
+
 
 //Подсчетом
     return 0;
