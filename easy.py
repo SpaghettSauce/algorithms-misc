047ca460a412c9f31269e5a7726719cd26ab5a00
Даня Ходосевич <114701605+SpaghettSauce@users.noreply.github.com>
2025-04-17 23:39:22 +0200

    misc

--- easy.py
+++ easy.py
@@ -1,3 +1,17 @@
-A = int(input())
-B = int(input())
-print(A+B)
+import matplotlib.pyplot as plt
+t = [x/100 for x in range(-100, 200)]
+aprior_est = 22
+def F(x, t):
+    val = t * (2 + abs(x))** (0.5) - t**2
+    return val
+x = []
+for i in range(len(t)):
+    cur_val_for_x = 2
+    for j in range(aprior_est):
+        cur_val_for_x = F(cur_val_for_x, t[i])
+    x.append(cur_val_for_x)
+plt.plot(t, x)
+plt.xlabel('t')
+plt.ylabel('x(t)')
+plt.grid(True)
+plt.show()
